59a6c38b4f80352dc36bc316039211a6c1408693
Aceep <alycia.gautier@laposte.net>
2023-08-03 10:25:48 +0200

    savior

--- ex01/Contact.cpp
+++
@@ -1,8 +0,0 @@
-#include "Contsact.hpp"
-
-void    Contact::create_contact()
-{
-    Contact new;
-    std::cout << "First name :";
-    std::getline(std::cin, new->_first_name);
-}
---
+++ ex01/src/main.cpp
@@ -0,0 +1,26 @@
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+int main()
+{
+    std::string input;
+    PhoneBook PhoneBook;
+    bool    run = true;
+
+    std::cout << "Welcome to the PhoneBook !" << PhoneBook.show_instruction() << std::endl;
+    while (run && std::getline(std::cin, input))
+    {
+        if (input.compare("ADD") == 0)
+            PhoneBook.set_information();
+        else if (input.compare("SEARCH") == 0)
+            PhoneBook.get_information();
+        else if (input.compare("EXIT") == 0)
+        {
+            std::cout << "Bye bye !" << std::endl;
+            run = false;
+            break ;
+        }
+        input.clear();    
+    }
+    return (0);
+}
